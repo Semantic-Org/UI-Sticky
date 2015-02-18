a6099ad939236aa66228c53dcc990fe800f5a62f
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:26:32 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-sticky',
+  summary : 'Semantic UI - Sticky (official): Single component release of sticky',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Sticky.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
