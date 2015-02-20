6cbb130996f2473b8b2c15bc373fa22ec8185928
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:16:26 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-sticky',
-  summary : 'Semantic UI - Sticky (official): Single component release of sticky',
-  version : '1.9.2',
+  summary : 'Semantic UI - Sticky: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Sticky.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'sticky.css',
+    'sticky.js'
+  ], 'client');
 });
