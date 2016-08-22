703005f75286d1b8f3ec9ecffbc3554aa96cdbc5
Pusher Robot <jack@semantic-ui.com>
2016-08-21 23:34:23 -0400

    Updated component to version 2.2.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-sticky',
   summary : 'Semantic UI - Sticky: Single component release',
-  version : '2.2.2',
+  version : '2.2.3',
   git     : 'git://github.com/Semantic-Org/UI-Sticky.git',
 });
 
